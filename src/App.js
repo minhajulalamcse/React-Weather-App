81bf45d418f6f17d087e5cc102422114fe4b6a21
Minhaj <minhajulalam.cse@gmail.com>
2020-09-07 17:41:13 +0600

    api added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Current from "./components/current";
-import Forecast from "./components/forecast";
-
+import axios from "axios";
 class App extends Component {
-  state = {};
   render() {
     return (
       <div className="container App">
         <h3 className="heading indigo-text">Weather App</h3>
-        <div className="row card">
+        <div className="card">
           <Current />
-          <Forecast />
         </div>
       </div>
     );
--- src/components/current.jsx
+++ src/components/current.jsx
@@ -1,46 +1,93 @@
 import React from "react";
+import axios from "axios";
+class Current extends React.Component {
+  state = {
+    current: null,
+  };
+  componentDidMount() {
+    axios
+      .get(
+        "http://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=8205bdb71cd8d86e6f7e21451a9ba64a&units=metric"
+      )
+      .then((response) => {
+        this.setState({
+          current: response.data,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  render() {
+    return (
+      <div className="center">
+        <div className="container">
+          <p className="flow-text">
+            <i className="fa fa-map-marker"></i>{" "}
+            <b>
+              {this.state.current ? (
+                <b>
+                  {this.state.current.name}, {this.state.current.sys.country}
+                </b>
+              ) : null}
+            </b>
+          </p>
 
-const Current = (props) => {
-  return (
-    <div className="col s6">
-      <div className="container">
-        <p className="flow-text">
-          <i className="fa fa-map-marker"></i> <b>Samair, BD</b>
-        </p>
-        <p className="gray-text">10:19pm, Saturday, August 29, 2020</p>
-
-        <p className="currentTemperature">
-          <i className="fa fa-sun-o"></i> 28&#8451;
-        </p>
-        <p>
-          <b>Feels like 31&#8451;.</b> <b>Moderate rain.</b> <b>Light breeze</b>
-        </p>
-        <div className="row">
-          <div className="col s6">
-            <p>
-              <span className="fa fa-send"></span> <span>2.6m/s SE</span>
-            </p>
-          </div>
-          <div className="col s6">
-            <p>
-              <span className="fa fa-compass"> </span> <span>1004hPa</span>
-            </p>
-          </div>
-          <div className="col s6">
-            <p>
-              <span className="fa fa-circle"> </span> <span>Humidity: 74%</span>
-            </p>
-          </div>
-          <div className="col s6">
-            <p>
-              <span className="fa fa-eye"> </span>{" "}
-              <span>Visibility: 3.5km</span>
-            </p>
+          <p className="currentTemperature">
+            {this.state.current ? this.state.current.main.temp : null}
+            &#8451;
+          </p>
+          <p>
+            <b>
+              Feels like{" "}
+              {this.state.current ? this.state.current.main.feels_like : null}
+              &#8451;.
+            </b>
+          </p>
+          <div className="row">
+            <div className="col s6">
+              <p>
+                <span className="fa fa-send"></span>{" "}
+                <span>
+                  Speed:{" "}
+                  {this.state.current ? this.state.current.wind.speed : null}
+                  2.6m/s
+                </span>
+              </p>
+            </div>
+            <div className="col s6">
+              <p>
+                <span className="fa fa-compass"> </span>{" "}
+                <span>
+                  Pressure:{" "}
+                  {this.state.current ? this.state.current.main.pressure : null}
+                  hPa
+                </span>
+              </p>
+            </div>
+            <div className="col s6">
+              <p>
+                <span className="fa fa-circle"> </span>{" "}
+                <span>
+                  Humidity:{" "}
+                  {this.state.current ? this.state.current.main.humidity : 0}%
+                </span>
+              </p>
+            </div>
+            <div className="col s6">
+              <p>
+                <span className="fa fa-eye"> </span>{" "}
+                <span>
+                  Visibility:{" "}
+                  {this.state.current ? this.state.current.visibility : 0}m
+                </span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Current;
